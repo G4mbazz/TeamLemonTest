dc2f859d0893c6eb0aa338d2de4ddc2ebfe2bf87
Voxuel <Leo.Fridh@hotmail.com>
2022-11-22 10:28:52 +0100

    Add project files.

---
+++ TeamLemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamLemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
