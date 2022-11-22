2dfbaf296de8aaaea1e8d6193809cf675bed7b2c
Voxuel <Leo.Fridh@hotmail.com>
2022-11-22 11:19:22 +0100

    Add login

--- TeamLemon/Controls/LoginClass.cs
+++ TeamLemon/Controls/LoginClass.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TeamLemon.Models;
 
 namespace TeamLemon.Controls
 {
     class LoginClass
     {
-        public void Login()
+        public void Login(Dictionary<int,User> allUsers)
         {
-
+            
         }
     }
 }
