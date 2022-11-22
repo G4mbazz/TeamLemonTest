8ae30b8bf2923347b124e0c52b11bd97df9b833c
Voxuel <Leo.Fridh@hotmail.com>
2022-11-22 12:24:34 +0100

    bug fix on display wrong user details in login

--- TeamLemon/Controls/LoginClass.cs
+++ TeamLemon/Controls/LoginClass.cs
@@ -8,7 +8,7 @@ namespace TeamLemon.Controls
 {
     class LoginClass
     {
-        public void Login(Dictionary<int, Person> allUsers)
+        public static void Login(Dictionary<int, Person> allUsers)
         {
             bool ok = false;
 
@@ -30,26 +30,22 @@ namespace TeamLemon.Controls
                 }
             } while (ok == false);
             }
-        private Person LoginValidation(Dictionary<int,Person> allUsers,string username, string password)
+        private static Person LoginValidation(Dictionary<int,Person> allUsers,string username, string password)
         {
             bool found = false;
             Person current = new Person();
             foreach (var item in allUsers)
             {
-                if (item.Value.Name == username && item.Value.Password == password && item.Value.LoginAttempts != 0)
+                if (item.Value.Name == username && item.Value.Password == password)
                 {
                     found = true;
                     current = item.Value;
                     break;
                 }
-                else
-                {
-                    Console.WriteLine("Wrong username or password");
-                    item.Value.LoginAttempts--;
-                }
             }
             if(found == true && current.LoginAttempts != 0)
             {
+                Console.WriteLine("FOUND");
                 return current;
             }
             else
--- TeamLemon/Program.cs
+++ TeamLemon/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+using TeamLemon.Models;
+using TeamLemon.Controls;
 namespace TeamLemon
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var dic = Admin.allUsers();
+
+            LoginClass.Login(dic);
         }
     }
 }
