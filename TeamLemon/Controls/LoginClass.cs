15c9a005485b88587490304273d8a896d21c0bf0
Patrik Skattberg <patrikskattberg@hotmail.com>
2022-11-24 11:08:04 +0100

    excessCodeRemoved & nameChanges

--- TeamLemon/Controls/LoginClass.cs
+++ TeamLemon/Controls/LoginClass.cs
@@ -22,8 +22,8 @@ namespace TeamLemon.Controls
         public static void LoginValidation(Dictionary<int, User> allUsers,Dictionary<int,Admin> allAdmins)
         {
             var menus = new MenuClass();
-            bool ok = false;
-            bool found = false;
+            bool LogIn = false;
+            bool UserFound = false;
             var currentUser = new User();
             var currentAdmin = new Admin();
             do
@@ -38,9 +38,9 @@ namespace TeamLemon.Controls
                 {
                     if (user.Value.Name == username && user.Value.Password == password && user.Value.LockedUser == false)
                     {
-                        found = true;
+                        UserFound = true;
                         currentUser = user.Value;
-                        ok = true;
+                        LogIn = true;
                         user.Value.LogInAttempt = 3;
                         user.Value.LockedUser = false;
                         break;
@@ -56,24 +56,20 @@ namespace TeamLemon.Controls
                 {
                     if (admin.Value.Name == username && admin.Value.Password == password)
                     {
-                        found = true;
+                        UserFound = true;
                         currentAdmin = admin.Value;
-                        ok = true;
+                        LogIn = true;
                         break;
                     }
                 }
                 if (currentUser.LogInAttempt <= 0 && currentAdmin.IsAdmin != true)
                 {
-                    currentUser.LockedUser = true;
-                    ok = false;
-                }
-                if (currentUser.LockedUser && currentAdmin.IsAdmin != true)
-                {
                     Console.WriteLine("The user is locked");
-                    ok = false;
+                    currentUser.LockedUser = true;
+                    LogIn = false;
                     continue;
                 }
-            } while (ok == false);
+            } while (LogIn == false);
             if (currentAdmin.IsAdmin == true)
             {
                 menus.AdminMenu(currentAdmin);
