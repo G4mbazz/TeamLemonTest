a9f55a47cd95a7f1cb64548c14ab5002cfbd3af8
Voxuel <Leo.Fridh@hotmail.com>
2022-12-05 14:24:56 +0100

    Add branch

---
+++ TeamLemon/Controls/LoanMangement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamLemon.Models;
+
+namespace TeamLemon.Controls
+{
+    public class LoanMangement
+    {
+        public static void TakeALoan()
+        {
+
+        }
+    }
+}
